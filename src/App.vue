e286916e7e4f0b6766c8f5562f946c94e75106c7
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-20 21:21:19 -0300

    tentativa de embelezamento

--- src/App.vue
+++ src/App.vue
@@ -14,8 +14,33 @@ import { routes } from './router'
   </main>
 </template>
 
-<style scoped>
+<style>
+header {
+  text-align: right;
+}
+
 #navbar {
   text-transform: capitalize;
+}
+
+h1 {
+  text-align: center;
+}
+
+div.form {
+  width: 80vw;
+  margin: 1vh 0px;
+  display: flex;
+  justify-content: center;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+
+ul li {
+  text-align: center;
 }
 </style>
