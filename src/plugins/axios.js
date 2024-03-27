1800b4e7ed8d15fd2fcb020d9caa16dacf1e38a3
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-26 23:34:37 -0300

    autenticação

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,4 +2,5 @@ import axios from "axios";
 
 // axios.defaults.baseURL = "http://0.0.0.0:19003/api";
 axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.headers.common = { 'Authorization': `bearer ${token}` }
 // axios.defaults.baseURL = "https://livraria-drf.herokuapp.com/api";
