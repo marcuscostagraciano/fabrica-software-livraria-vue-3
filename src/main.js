2d3a4b3a558b232e30a06c4a1b3ad32c0db1f434
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-26 23:36:32 -0300

    separação creação vuetify

--- src/main.js
+++ src/main.js
@@ -2,24 +2,14 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
-import { router } from './router';
 import './plugins/axios';
 
 import './assets/main.css';
-
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-    components,
-    directives,
-})
+import { router } from './router';
+import { vuetify } from './plugins/vuetify';
 
 const app = createApp(App);
-''
+
 app.use(createPinia());
 app.use(vuetify)
 app.use(router);
