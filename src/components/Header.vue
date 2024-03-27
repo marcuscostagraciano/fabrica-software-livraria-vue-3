10e9588e24802dff42ff7ca84e3647f634208d4f
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-03-26 23:32:23 -0300

    remoção componentes não utilizados

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { routes } from '@/router'
+
+</script>
+
+<template>
+    <header>
+        <a v-for="route in routes" id="navbar">
+            <router-link :to="route.path">{{ route.name }}</router-link> |
+        </a>
+    </header>
+</template>
+
+<style scoped></style>
